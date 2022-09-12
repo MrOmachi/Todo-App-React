40d1a1093c9b794e8d0d9905e87a8fb6a79c36c1
Omachi John <omachi.john@.com>
2022-09-12 20:34:02 +0100

    Add todolist component for todo-list app

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,22 +1,22 @@
-// import React, { Component } from "react";
-// import TodoItem from "./TodoItem";
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
 
-// class TodosList extends Component {
-//   render() {
-//     return (
-//       <ul>
-//         {this.props.todos.map((todo) => (
-//           <TodoItem
-//             key={todo.id}
-//             todo={todo}
-//             handleChangeProps={this.props.handleChangeProps}
-//             deleteTodoProps={this.props.deleteTodoProps}
-//             setUpdate={this.props.setUpdate}
-//           />
-//         ))}
-//       </ul>
-//     );
-//   }
-// }
+class TodosList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+            setUpdate={this.props.setUpdate}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
 
-// export default TodosList;
+export default TodosList;
